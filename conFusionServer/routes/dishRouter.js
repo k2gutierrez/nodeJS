7152950fc9a0b8bd8a346faf62c7292cdef30b02
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-01-05 12:53:44 -0600

    assignment 3

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -14,14 +14,14 @@ dishRouter.route('/')
 .get((req, res, next) => {
     Dishes.find({})
     .populate('comments.author')
-    .then((dish) => {
+    .then((dishes) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.json(dish);
+      res.json(dishes);
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
       console.log('Dish Created ', dish);
@@ -31,11 +31,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.remove({})
     .then((resp) => {
       res.statusCode = 200;
@@ -56,11 +56,11 @@ dishRouter.route('/:dishId')
   }, (err) => next(err))
   .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   res.statusCode = 403;
   res.end('POST operation not supported on /dishes/' + req.params.dishId);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Dishes.findByIdAndUpdate(req.params.dishId, {
     $set: req.body
   }, { new: true })
@@ -71,7 +71,7 @@ dishRouter.route('/:dishId')
   }, (err) => next(err))
   .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Dishes.findByIdAndRemove(req.params.dishId)
   .then((resp) => {
     res.statusCode = 200;
@@ -128,7 +128,7 @@ dishRouter.route('/:dishId/comments')
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes/' + req.params.dishId + '/comments');
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
       if (dish != null) {
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,55 +1,79 @@
-var express = require('express');
+let express = require('express');
 const bodyParser = require('body-parser');
 let User = require('../models/user');
 let passport = require('passport');
 let authenticate = require('../authenticate');
 
-var router = express.Router();
+let router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content_type', 'application/json');
+      res.json(users);
+    }
+  })
 });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) =>  {
-    if (err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      if (req.body.firstname)
-        user.firstname = req.body.firstname;
-      if (req.body.lastname)
-        user.lastname = req.body.lastname;
-      user.save((err, user) => {
-        if (err) {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({err: err});
-          return ;
-        }
-
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful'});
+  User.register(new User({
+      username: req.body.username
+    }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          err: err
         });
-      });
-    }
-  });
+      } else {
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err, user) => {
+          passport.authenticate('local')(req, res, () => {
+            if (err) {
+              res.statusCode = 500;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({
+                err: err
+              });
+              return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+              success: true,
+              status: 'Registration Successful!'
+            });
+          });
+        });
+      }
+    });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res, next) => {
-  
-  let token = authenticate.getToken({_id: req.user._id})
-  
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  let token = authenticate.getToken({
+    _id: req.user._id,
+    firstname: req.user.firstname,
+    lastname: req.user.lastname
+  });
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in'});
+  res.json({
+    success: true,
+    status: 'You are successfully logged in!',
+    token: token
+  });
 });
 
 router.get('/logout', (req, res) => {
@@ -59,7 +83,7 @@ router.get('/logout', (req, res) => {
     res.redirect('/');
   }
   else {
-    var err = new Error('You are not logged in!');
+    let err = new Error('You are not logged in!');
     err.status = 403;
     next(err);
   }
